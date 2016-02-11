bce725446691cd79dafe281dfc564dc2bb359230
Mario Garcia <alexander.garcia18@yahoo.com>
2016-02-10 19:41:47 -0800

    Update background.js

--- public/scripts/js/background.js
+++ public/scripts/js/background.js
@@ -3,6 +3,7 @@ var imgObj = null;
 var escape = false;
 var button_down = false;
 
+// Initialize the image object onto the html file.
 function init() {
     imgObj = document.createElement('myImage');
     imgObj.innerHTML = "<img src=\"/imgs/18254.jpg\" height=\"100\" width=\"100\" />";
@@ -11,15 +12,18 @@ function init() {
     document.body.appendChild(imgObj);
 }
 
+// Lets move our image to the right.
 function moveRight() {
     imgObj.style.left = parseInt(imgObj.style.left) + 10 + 'px';
     socket.emit('move right', { });
 }
 
+// Moves the image left, yet this feature is not yet functional.
 function moveLeft() {   
     imgObj.style.right = parseInt(imgObj.style.right) + 10 + 'px';
 }
 
+// None of these are functional.
 function moveUp() {
     imgObj.style.top = parseInt(imgObj.style.top) + 10 + 'px';
 }
