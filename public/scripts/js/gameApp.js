cceb2b58dcf1e01753e8488b8d52fdc7149d70c1
CheezBoiger <alexander.garcia18@yahoo.com>
2016-02-20 17:51:59 -0800

    latch gameApp.js

--- public/scripts/js/gameApp.js
+++ public/scripts/js/gameApp.js
@@ -1,13 +1,61 @@
-var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+var socket = io();
 
-function preload() {
+window.onload = function () {
+    var game = new Phaser.Game(1920, 1080, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+    var cursors;
+    var player;
+    function preload() {
+        game.load.image('cat', '/assets/18254.jpg');
+    }
 
-}
+    function create() {
+        player = game.add.sprite(0, 0, 'cat');
+        player.width = 550;
+        player.height = 350;
+        game.physics.enable(player, Phaser.Physics.ARCADE);
+        cursors = game.input.keyboard.createCursorKeys();
+    }
 
-function create() {
+    function update() {
+        player.body.velocity.x = 0;
+        player.body.velocity.y = 0;
+        
+        if (cursors.left.isDown && cursors.up.isDown) {
+            player.body.velocity.x = -150;
+            player.body.velocity.y = -150;
+        } else if (cursors.left.isDown && cursors.down.isDown) {
+            player.body.velocity.x = -150;
+            player.body.velocity.y = 150;
+        } else if (cursors.left.isDown) {
+            player.body.velocity.x = -150;
+        } else if (cursors.right.isDown && cursors.up.isDown) {
+            player.body.velocity.x = 150;
+            player.body.velocity.y = -150;
+        } else if (cursors.right.isDown && cursors.down.isDown) {
+            player.body.velocity.x = 150;
+            player.body.velocity.y = 150;
+        } else if (cursors.right.isDown) {
+            player.body.velocity.x = 150;
+        } else if (cursors.down.isDown) {
+            player.body.velocity.y = 150;
+        } else if (cursors.up.isDown) {
+            player.body.velocity.y = -150;
+        } 
 
-}
+        socket.on('move right', function (data) {
+            player.body.velocity.x = 750;
+        });
 
-function update() {
+        socket.on('move left', function (data) {
+            player.body.velocity.x = -750;
+        });
 
-}
+        socket.on('move up', function (data) {
+            player.body.velocity.y = -750;
+        });
+
+        socket.on('move down', function (data) {
+            player.body.velocity.y = 750;
+        });
+    }
+};
